79d60c664b60600eb43bfc7f805873d88274ecd1
Hussain <jakirdeveloper@gmail.com>
2024-07-17 01:06:50 +0530

    init

---
+++ src/main.tsx
@@ -0,0 +1,30 @@
+import "./styles/global.scss";
+
+import { createRouter,RouterProvider } from "@tanstack/react-router";
+import { StrictMode } from "react";
+import ReactDOM from "react-dom/client";
+
+// Import the generated route tree
+import { routeTree } from "./routeTree.gen";
+
+// Register the router instance for type safety
+declare module "@tanstack/react-router" {
+  interface Register {
+    router: typeof router;
+  }
+}
+
+const router = createRouter({
+  routeTree,
+});
+
+// Render the app
+const rootElement = document.getElementById("root")!;
+if (!rootElement.innerHTML) {
+  const root = ReactDOM.createRoot(rootElement);
+  root.render(
+    <StrictMode>
+      <RouterProvider router={router} />
+    </StrictMode>
+  );
+}
---
+++ vite.config.ts
@@ -0,0 +1,15 @@
+import { TanStackRouterVite } from "@tanstack/router-vite-plugin";
+import react from "@vitejs/plugin-react";
+import path from "path";
+import { defineConfig } from "vite";
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react(), TanStackRouterVite()],
+  resolve: {
+    alias: {
+      "react-native": "react-native-web",
+      "@": path.resolve(__dirname, "src"),
+    },
+  },
+});
